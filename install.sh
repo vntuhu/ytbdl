adcf24e1976e17ab5713cab223114e0ff6b2ad2b
vntuhu <91951260+vntuhu@users.noreply.github.com>
2021-10-06 13:32:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,64 +21,49 @@ clear
 
 echo -e $B $L"YouTube-dl Installer By"  $R "VNTuhu" $N
 echo -e $B $L"Installing python..." $N
-
-		pkg install python -y
-		clear
+pkg install python -y
+clear
 
 echo -e $B $L"YouTube-dl Installer By"  $R "VNTuhu" $N
 echo -e $B $L"Installing ffmpeg..." $N
-
-		apt install ffmpeg -y
-		apt update -y
-		clear
+apt install ffmpeg -y
+apt update -y
+clear
 
 echo -e $B $L"YouTube-dl Installer By"  $R "VNTuhu" $N
 echo -e $B $L"Installing wget..." $N
-
-		pkg install wget -y
-        clear
-
+pkg install wget -y
+clear
 echo -e $B $L"YouTube-dl Installer By"  $R "VNTuhu" $N
 echo -e $B $L"Installing YouTube-dl..." $N 
-
-		pip install youtube-dl
-		clear
+pip install youtube-dl
+clear
 
 echo -e $B $L"YouTube-dl Installer By"  $R "VNTuhu" $N
 echo -e $B $L"Setting up configs..." $N
 sleep 2
-echo -e $B $L "Creating Youtube folder..." $N
-		mkdir /data/data/com.termux/files/home/storage/shared/Youtube
-echo -e $B $L "Creating Music folder..." $N
-		mkdir /data/data/com.termux/files/home/storage/shared/Music
-echo -e $B $L "Creating Movies folder..." $N
-		mkdir /data/data/com.termux/files/home/storage/shared/Moviesecho -e $B $L "Creating Movies folder..." $N
+echo -e $B $L"Creating folder..." $N
+mkdir /data/data/com.termux/files/home/storage/shared/Youtube
+mkdir /data/data/com.termux/files/home/storage/shared/Music/
+mkdir /data/data/com.termux/files/home/storage/shared/Movies/
+mkdir ~/bin
+mkdir -p ~/.config/youtube-dl
 clear
-sleep 1.5
 
 echo -e $B $L"YouTube-dl Installer By"  $R "VNTuhu" $N
-echo -e $B $L "Remove old file..." $N
+echo -e $B $L"Remove old file..." $N
 rm -r /data/data/com.termux/files/home/.config/youtube-dl/config
 rm -r /data/data/com.termux/files/home/bin/termux-url-opener
-
-echo -e $B $L "Creating youtube-dl config folder..." $N
-		mkdir -p ~/.config/youtube-dl
-sleep 1.5
+rm -r /data/data/com.termux/files/home/install.sh
+rm -r /data/data/com.termux/files/home/update.sh
 
 echo -e $B $L "Getting config file..." $N
-		wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/config -P /data/data/com.termux/files/home/.config/youtube-dl
-
-echo -e $B $L "Creating bin folder..." $N
-		mkdir ~/bin
-sleep 1.5
-echo -e $B $L "Getting termux-url-opener files..." $N
-		wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/termux-url-opener -P /data/data/com.termux/files/home/bin
-sleep 1.5
+wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/config -P /data/data/com.termux/files/home/.config/youtube-dl
+wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/termux-url-opener -P /data/data/com.termux/files/home/bin
 wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/update.sh -P /data/data/com.termux/files/home
 wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/install.sh -P /data/data/com.termux/files/home
-		clear
-		
+clear
+echo -e $B $L"YouTube-dl Installer By"  $R "VNTuhu" $N
 echo -e $B $L "Finishing configuration..." $N
 sleep 5
-
 echo -e $G"Installation Finished..." $N
