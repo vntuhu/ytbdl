311c00bd1e59e232657fc999dc9231fdb89051e4
vntuhu <91951260+vntuhu@users.noreply.github.com>
2021-10-06 09:15:08 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-rm -r /bin/termux-url-opener | wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/termux-url-opener -P /data/data/com.termux/files/home/bin | rm -r /.config/youtube-dl/config | 
+rm -r /bin/termux-url-opener | wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/termux-url-opener -P /data/data/com.termux/files/home/bin | rm -r /.config/youtube-dl/config | wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/config -P /data/data/com.termux/files/home/.config/youtube-dl
