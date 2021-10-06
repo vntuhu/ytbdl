2b4acf2238f3afb677d62f4264ecb6fd2e538698
vntuhu <91951260+vntuhu@users.noreply.github.com>
2021-10-06 13:07:08 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,1 +1,9 @@
-rm -r /data/data/com.termux/files/home/bin/termux-url-opener | wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/termux-url-opener -P /data/data/com.termux/files/home/bin | rm -r /data/data/com.termux/files/home/.config/youtube-dl/config | wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/config -P /data/data/com.termux/files/home/.config/youtube-dl
+rm -r /data/data/com.termux/files/home/bin/termux-url-opener
+wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/termux-url-opener -P /data/data/com.termux/files/home/bin
+rm -r /data/data/com.termux/files/home/.config/youtube-dl/config
+wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/config -P /data/data/com.termux/files/home/.config/youtube-dl
+rm -r /data/data/com.termux/files/home/install.sh
+wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/install.sh -P /data/data/com.termux/files/home/install.sh
+rm -r /data/data/com.termux/files/home/install.sh
+wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/update.sh -P /data/data/com.termux/files/home/update.sh
+echo "Done..."
