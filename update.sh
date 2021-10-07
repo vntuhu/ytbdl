00e328ed418d8870b8dce3103c25efa980a03737
vntuhu <91951260+vntuhu@users.noreply.github.com>
2021-10-08 01:41:02 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ rm -r /data/data/com.termux/files/home/.config/youtube-dl/config
 wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/config -P /data/data/com.termux/files/home/.config/youtube-dl
 rm -r /data/data/com.termux/files/home/install.sh
 wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/install.sh -P /data/data/com.termux/files/home/
-#rm -r /data/data/com.termux/files/home/update.sh
+rm -r /data/data/com.termux/files/home/update.sh
 wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/update.sh -P /data/data/com.termux/files/home/
 rm -r /data/data/com.termux/files/usr/bin/ytb
 wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/ytb -P /data/data/com.termux/files/usr/bin/
