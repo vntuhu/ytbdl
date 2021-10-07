93149139ab96413e26de9e545a9f5ed3c31ef631
vntuhu <91951260+vntuhu@users.noreply.github.com>
2021-10-08 01:27:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -64,6 +64,7 @@ wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/termux-url-opener -P /d
 wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/update.sh -P /data/data/com.termux/files/home
 wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/install.sh -P /data/data/com.termux/files/home
 wget https://raw.githubusercontent.com/vntuhu/ytbdl/main/ytb -P /data/data/com.termux/files/usr/bin
+chmod +x /data/data/com.termux/files/usr/bin/ytb
 clear
 echo -e $B $L"YouTube-dl Installer By"  $R "VNTuhu" $N
 echo -e $B $L "Finishing configuration..." $N
