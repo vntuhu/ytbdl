fb2fd525391cc2d8da52ff82ce1cc9d78983e5ea
vntuhu <91951260+vntuhu@users.noreply.github.com>
2022-01-19 07:04:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,38 +3,38 @@ main=https://raw.githubusercontent.com/vntuhu/ytbdl/main
 home=/data/data/com.termux/files/home
 bin=/data/data/com.termux/files/usr/bin
 
-echo "YouTube-dl Installer ByVNTuhu"
+echo "YouTube-dl Installer By VNTuhu"
 termux-setup-storage
 apt update -y
 clear
 
-echo "YouTube-dl Installer ByVNTuhu"
+echo "YouTube-dl Installer By VNTuhu"
 echo "Installing python..." 
 pkg install python -y
 clear
 
-echo "YouTube-dl Installer ByVNTuhu"
+echo "YouTube-dl Installer By VNTuhu"
 echo "Installing ffmpeg..." 
 apt install ffmpeg -y
 apt update -y
 clear
 
-echo "YouTube-dl Installer ByVNTuhu"
+echo "YouTube-dl Installer By VNTuhu"
 echo "Installing wget..." 
 pkg install wget -y
 clear
-echo "YouTube-dl Installer ByVNTuhu"
+echo "YouTube-dl Installer By VNTuhu"
 echo "Installing git..." 
 pkg install git -y
 clear
-echo "YouTube-dl Installer ByVNTuhu"
+echo "YouTube-dl Installer By VNTuhu"
 echo "Installing yt-dlp..."  
 python3 -m pip install --upgrade git+https://github.com/yt-dlp/yt-dlp.git@release
 wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O $bin/yt-dlp
 chmod a+rx $bin/yt-dlp
 termux-fix-shebang $PREFIX/bin/yt-dlp
 clear
-echo "YouTube-dl Installer ByVNTuhu"
+echo "YouTube-dl Installer By VNTuhu"
 
 # mkdir
 mkdir ~/bin
