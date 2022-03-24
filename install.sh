9938d3a69a366e5d59e29a1846d5077aa4eb2a23
vntuhu <91951260+vntuhu@users.noreply.github.com>
2022-03-24 16:50:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,41 +5,15 @@ bin=/data/data/com.termux/files/usr/bin
 echo "yt-dlp Installer By VNTuhu"
 termux-setup-storage
 pkg update -y
-clear
-
-echo "yt-dlp Installer By VNTuhu"
-echo "Installing python..." 
 pkg install python -y
-clear
-
-echo "yt-dlp Installer By VNTuhu"
-echo "Installing ffmpeg..." 
 pkg install ffmpeg -y
-clear
-
-echo "yt-dlp Installer By VNTuhu"
-echo "Installing wget..." 
 pkg install wget -y
-clear
-
-echo "yt-dlp Installer By VNTuhu"
-echo "Installing git..." 
 pkg install git -y
-clear
-
-echo "yt-dlp Installer By VNTuhu"
-echo "Installing yt-dlp..."  
 python3 -m pip install --upgrade git+https://github.com/yt-dlp/yt-dlp.git@release
 wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -O $bin/yt-dlp
 chmod a+rx $bin/yt-dlp
-clear
-
-echo "yt-dlp Installer By VNTuhu"
-
-# mkdir
 mkdir ~/bin
 wget $main/updt -P $bin
 chmod +x $bin/updt
 updt
-clear
 echo "Installation Finished..." 
